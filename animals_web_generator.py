b6d965ceef195969dd713475f81a41acaa5d0042
Mark Lünnemann <Mark@705d9b6d-b98e-4901-86d5-90197f9651cd.fritz.box>
2025-12-19 23:28:48 +0100

    Insert animal data into HTML template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,23 +5,37 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+# Load animal data
 animals_data = load_data('animals_data.json')
 
+# Read HTML template
+with open("animals_template.html", "r") as file:
+    template_html = file.read()
+
+# Animal info string
+animals_info = ""
 
 for animal in animals_data:
     if "name" in animal:
-        print(f"Name: {animal['name']}")
+        animals_info += f"Name: {animal['name']}\n"
 
     characteristics = animal.get("characteristics", {})
 
     if "diet" in characteristics:
-        print(f"Diet: {characteristics['diet']}")
+        animals_info += f"Diet: {characteristics['diet']}\n"
 
     if "locations" in animal and len(animal["locations"]) > 0:
-        print(f"Locations: {animal['locations'][0]}")
+        animals_info += f"Locations: {animal['locations'][0]}\n"
 
     if "type" in characteristics:
-        print(f"Type: {characteristics['type']}")
+        animals_info += f"Type: {characteristics['type']}\n"
 
     print()
 
+# Replace placeholder in template
+final_html = template_html.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+# Write final HTMl file
+
+with open("animals.html", "w") as file:
+    file.write(final_html)
