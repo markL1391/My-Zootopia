b6564f32bbe7ecc190c05c765fabe1555f311bb0
Mark Lünnemann <Mark@057f355a-9de7-4aa8-bbb6-421372658aaf.fritz.box>
2025-12-19 22:29:28 +0100

    Read and display data from JSON file

---
+++ animals_web_generator.py
@@ -0,0 +1,27 @@
+import json
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+animals_data = load_data('animals_data.json')
+
+
+for animal in animals_data:
+    if "name" in animal:
+        print(f"Name: {animal['name']}")
+
+    characteristics = animal.get("characteristics", {})
+
+    if "diet" in characteristics:
+        print(f"Diet: {characteristics['diet']}")
+
+    if "locations" in animal and len(animal["locations"]) > 0:
+        print(f"Locations: {animal['locations'][0]}")
+
+    if "type" in characteristics:
+        print(f"Type: {characteristics['type']}")
+
+    print()
+
